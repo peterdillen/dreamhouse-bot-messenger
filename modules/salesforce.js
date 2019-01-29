e70f8ab0f748fda314ac277de1bce187927b1820
Peter Dillen <peter.dillen@be.ibm.com>
2019-01-29 17:00:47 +0100

    Update salesforce.js

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -10,7 +10,7 @@ let nforce = require('nforce'),
 let org = nforce.createConnection({
     clientId: SF_CLIENT_ID,
     clientSecret: SF_CLIENT_SECRET,
-    redirectUri: 'http://127.0.0.1',   //'http://localhost:3000/oauth/_callback',
+    redirectUri: 'https://demosfbeer.herokuapp.com/webhook',   //'http://localhost:3000/oauth/_callback',
     mode: 'single',
     autoRefresh: true
 });
