308bf6de0464a35d7a39de43a2228c35b05f9fb3
Peter Dillen <peter.dillen@be.ibm.com>
2019-01-29 17:14:55 +0100

    Update salesforce.js

--- modules/salesforce.js
+++ modules/salesforce.js
@@ -4,8 +4,8 @@ let nforce = require('nforce'),
 
     SF_CLIENT_ID = '3MVG9fTLmJ60pJ5KmCpSrZO8.PDA46HNBJFSutUMlgMvrafU1wEjKQjEEjaglKy40pEvvN07NbhE_x4Kovk8y',   //process.env.SF_CLIENT_ID,
     SF_CLIENT_SECRET = '6999411422438489539',  //process.env.SF_CLIENT_SECRET,
-    SF_USER_NAME = 'integration@00d1t000000xlkneau.com',  //process.env.SF_USER_NAME,
-    SF_PASSWORD = 'integration@00d1t000000xlkneau.com';  //process.env.SF_PASSWORD;
+    SF_USER_NAME = 'peter.dillen@sf-messenger.demo',  //process.env.SF_USER_NAME,
+    SF_PASSWORD = 'Salesforce1VGO5aJAMjkSHOAbApwWPztVC';  //process.env.SF_PASSWORD;
 
 let org = nforce.createConnection({
     clientId: SF_CLIENT_ID,
