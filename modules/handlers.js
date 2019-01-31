8b2eac7e435034f8f667602c48698ba87f20508e
Peter Dillen <peter.dillen@be.ibm.com>
2019-01-31 09:47:48 +0100

    Update handlers.js

--- modules/handlers.js
+++ modules/handlers.js
@@ -62,7 +62,8 @@ exports.hi = (sender) => {
 exports.duvel = (sender) => {
   messenger.send({text: `So you like Duvel? Let met show other beers you may like.`}, sender);
   salesforce.findBeersSimilar('Duvel').then(properties => {
-      messenger.send(formatter.formatBeers(properties), sender);
+      //messenger.send(formatter.formatBeers(properties), sender);
+      messenger.send({text: `Found it.`}, sender);
   });
 };
 
